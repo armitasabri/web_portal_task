53494e973954e7dffb617a862c746d5d11b150ab
armitasabri <rmita.sbr@gmail.com>
2023-02-23 14:22:54 +0330

    bootstrap table to show tasks added to task view

--- views/task.php
+++ views/task.php
@@ -8,6 +8,39 @@
 
 <body>
     <div class="container">
+        <div class="section mt-2">
+            <div class="d-flex flex-column">
+                <table  id="myTable" class="table table-bordered table-responsive"  style="width:100%">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>task</th>
+                        <th>title</th>
+                        <th>description</th>
+                        <th>colorCode</th>
+                    </tr>
+                    </thead>
+                    <tbody  id="tableBody">
+
+                    <?php if(count($tasks)>1) : ?>
+                        <?php foreach ($tasks as $key => $rows) :?>
+                            <tr >
+                                <td><?php echo $key+1; ?></td>
+                                <td> <?php echo $rows['task']; ?></td>
+                                <td> <?php echo $rows['title']; ?></td>
+                                <td> <?php echo $rows['description']; ?></td>
+                                <td style="background-color:<?=$rows['colorCode']?>">
+                                </td>
+                            </tr>
+                        <?php endforeach;?>
+                    <?php endif;?>
+
+                    </tbody>
+                </table>
+
+
+            </div>
+        </div>
 
     </div>
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
