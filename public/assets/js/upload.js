81020e793f2782b46dca213d2f367665d78f2590
armitasabri <rmita.sbr@gmail.com>
2023-02-23 14:33:20 +0330

    upload.js refactored

--- public/assets/js/upload.js
+++ public/assets/js/upload.js
@@ -1,11 +1,5 @@
 (function () {
-    var input = document.getElementById("image"),
-        formdata = false;
-
-    if (window.FormData) {
-        formdata = new FormData();
-        document.getElementById("btn").style.display = "none";
-    }
+    var input = document.getElementById("image");
 
     function showUploadedItem (source) {
         var list = document.getElementById("image-list"),
@@ -17,12 +11,9 @@
         li.appendChild(img);
         list.appendChild(li);
     }
-
-
     if (input.addEventListener) {
         input.addEventListener("change", function (evt) {
             var i = 0, len = this.files.length, img, reader, file;
-            // document.getElementById("response").innerHTML = "Uploading . . ."
 
             for ( ; i < len; i++ ) {
                 file = this.files[i];
