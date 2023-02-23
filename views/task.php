a13b3d3aa5b97ea307bcef2870de8a566c427b72
armitasabri <rmita.sbr@gmail.com>
2023-02-23 14:49:24 +0330

    all script tags added to task.php

--- views/task.php
+++ views/task.php
@@ -94,6 +94,10 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="./public/assets/js/pagination.js"></script>
+    <script src="./public/assets/js/search.js"></script>
+    <script src="./public/assets/js/upload.js"></script>
+
 </body>
 </html>
 
