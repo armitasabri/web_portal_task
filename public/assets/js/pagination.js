37ac9fad6a1626562d33afd5e19ed1e812ebc36f
armitasabri <rmita.sbr@gmail.com>
2023-02-23 14:44:02 +0330

    pagination.js updated

---
+++ public/assets/js/pagination.js
@@ -0,0 +1,48 @@
+
+
+const my_body = document.getElementById("tableBody");
+const all_rows=my_body.rows;
+const numberOfItems = my_body.rows.length-1
+const numberPerPage = 5
+const currentPage = 1
+const numberOfPages = Math.ceil(numberOfItems/numberPerPage)
+
+
+function buildPage(currPage) {
+
+    const trimStart = (currPage-1)*numberPerPage
+    var trimEnd = trimStart + numberPerPage
+    if(currPage==numberOfPages){
+        trimEnd =my_body.rows.length
+    }
+    const old_tbody = document.getElementById("tableBody");
+    const new_tbody = document.createElement('tbody');
+    old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+    new_tbody.setAttribute("id", "tableBody");
+    var roww = [];
+
+    for(var k=trimStart;k<trimEnd;k++){
+
+        roww[k] = all_rows[k]
+
+    }
+    var array=[];
+
+    for(var z=0;z<numberPerPage ;z++){
+        array[z] = new_tbody.insertRow(z);
+        array[z].innerHTML =roww[z+trimStart].innerHTML;
+    }
+
+}
+
+function buildPagination(currPageNum, numberOfPages){
+    console.log('build me');
+    var list=[];
+    for (let i=0; i<numberOfPages; i++) {
+        list +="<li class='page-item'>"+
+            "<button class='page-link' >"+(currPageNum + i)+"</button></li>";
+    }
+    document.getElementById('pagination').innerHTML=list;
+}
+
+
