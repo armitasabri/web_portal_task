c87c546ac16f56c35941aec133b7c3fb77da751d
armitasabri <rmita.sbr@gmail.com>
2023-02-23 14:14:28 +0330

    curll codes to get tasks data from remote api added to returnedData method

--- app/Controllers/FetchTasksController.php
+++ app/Controllers/FetchTasksController.php
@@ -15,6 +15,31 @@ class FetchTasksController{
 
     protected function returnedData()
     {
+        $authorization=$this->getAccessToken();
+        $access_token=$authorization['oauth']['access_token'];
+        $url = "https://api.baubuddy.de/dev/index.php/v1/tasks/select";
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "Authorization: Bearer $access_token",
+                "Content-Type: application/json"
+            ],
+        ]);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            return false;
+        } else {
+            return json_decode($response,true);
+        }
 
     }
 
